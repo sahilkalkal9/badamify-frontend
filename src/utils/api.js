5721062ff3fc3988f098a4ceb30408de5a16cdc8
Sahil Kalkal <sahilkalkal108@gmail.com>
2026-05-15 03:12:31 +0530

    latest code + api code changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://badamify-backend.onrender.com/api",
+  baseURL: "https://badamify-backend-dvw2.onrender.com/api",
 });
 
 export default api;
